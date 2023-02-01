efc4afaaa1bb7bc0d47f5b1efd90b8cc288295f8
lucaspedemonti <lucaspedemonti91@gmail.com>
2023-01-31 23:12:29 -0300

    first commit

---
+++ lucas-tienda/src/components/header/Brand.js
@@ -0,0 +1,9 @@
+import brand from "../../img/logolp.png";
+
+const Brand = () => {
+    return(
+        <img src={brand} alt="Tienda LP" title="Tienda LP"></img>
+    )
+}
+
+export default Brand;
---
+++ lucas-tienda/src/components/header/CartWidget.js
@@ -0,0 +1,14 @@
+import cart from "../../img/cart.svg";
+
+const CartWidget = () => {
+    return(
+        <div>
+            <img src={cart} alt="cart"></img>
+            <span>
+                1
+            </span>
+        </div>        
+    )
+}
+
+export default CartWidget;
