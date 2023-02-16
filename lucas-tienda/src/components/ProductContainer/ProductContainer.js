35af3fabce0ba51a7eec501afe82eec2fd06ff25
Lucas Pedemonti <lucaspedemonti91@gmail.com>
2023-02-16 11:17:48 -0300

    Update Product

--- lucas-tienda/src/components/ProductContainer/ProductContainer.js
+++ lucas-tienda/src/components/ProductContainer/ProductContainer.js
@@ -5,27 +5,26 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-// import imgUser from "./asusrog1.jpg";
+import imgUser from "./asusrog1.jpg";
 
 const ProductContainer = () => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image={imgUser}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+            Mother Asus Rog Strix X570-e Wifi Ii
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+            Placa base de gaming ATX AMD X570 con PCIe® 4.0, 12+4 etapas de potencia, WiFi 6E (802.11ax), Ethernet de 2,5 Gb de Realtek, cancelación de ruido bidireccional con IA, dos M.2 con disipadores, SATA 6 GB/s, USB 3.2 Gen. 2 de tipo C e iluminación Aura Sync RGB
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
+        <Button size="small">$148.699</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
